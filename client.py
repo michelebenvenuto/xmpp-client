142965131c82ab895376b8fe91ee9314dbf6c797
michelebenveuto <35434145+michelebenveuto@users.noreply.github.com>
2021-07-27 14:58:44 -0600

    Starting the logging

---
+++ client.py
@@ -0,0 +1,39 @@
+
+import logging
+from getpass import getpass
+
+import slixmpp
+from argparse import ArgumentParser
+
+class UserClient(slixmpp.ClientXMPP):
+    
+    def __init__(self, jid, password):
+        super().__init__(jid, password)
+
+        self.add_event_handler('session_start', self.start)
+
+    async def start(self,event):
+        self.send_presence()
+        await self.get_roster()
+
+if __name__ =='__main__':
+    parser = ArgumentParser(description=UserClient.__doc__)
+
+    parser.add_argument("-j", dest="jid")
+    parser.add_argument("-p", dest="password")
+
+    args = parser.parse_args()
+
+    if args.jid is None:
+        args.jid = input("Username: ")
+    if args.password is None:
+        args.password = getpass("Password: ")
+    
+    xmpp = UserClient(args.jid, args.password)
+    xmpp.register_plugin('xep_0030') 
+    xmpp.register_plugin('xep_0004') 
+    xmpp.register_plugin('xep_0060') 
+    xmpp.register_plugin('xep_0199')
+
+    xmpp.connect()
+    xmpp.process(forever=False)
